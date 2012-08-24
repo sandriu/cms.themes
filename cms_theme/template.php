448c70c36bce3b09c707a1d0cb63ce6ab32b4cca
Cristian Romanescu <cristian.romanescu@eaudeweb.ro>
2012-08-24 21:14:01 +0300

    [#34780803] Override default theme's styles to obtain our CMS theme

--- cms_theme/template.php
+++ cms_theme/template.php
@@ -1,5 +1,82 @@
 <?php
 
 function cms_js_alter(&$javascript) {
-	$javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'cms').'/js/jquery-1.8.0.min.js';
+	$javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'CMS_theme').'/js/jquery-1.8.0.min.js';
+}
+
+
+/**
+ * Preprocess variables for page.tpl.php
+ *
+ * @see page.tpl.php
+ */
+function CMS_theme_preprocess_page(&$variables) {
+  // Add information about the number of sidebars.
+  if (!empty($variables['page']['sidebar_first']) && !empty($variables['page']['sidebar_second'])) {
+    $variables['columns'] = 3;
+  }
+  elseif (!empty($variables['page']['sidebar_first'])) {
+    $variables['columns'] = 2;
+  }
+  elseif (!empty($variables['page']['sidebar_second'])) {
+    $variables['columns'] = 2;
+  }
+  else {
+    $variables['columns'] = 1;
+  }
+
+  // Primary nav
+  $variables['primary_nav'] = FALSE;
+  if($variables['main_menu']) {
+    // Build links
+    $tree = menu_tree_page_data(variable_get('menu_main_links_source', 'main-menu'));
+    $variables['main_menu'] = twitter_bootstrap_menu_navigation_links($tree);
+    
+    // Build list
+    $variables['primary_nav'] = theme('twitter_bootstrap_links', array(
+      'links' => $variables['main_menu'],
+      'attributes' => array(
+        'class' => 'nav nav-pills'
+      ),
+    ));
+  }
+  
+  // Secondary nav
+  $variables['secondary_nav'] = FALSE;
+  if($variables['secondary_menu']) {
+    $secondary_menu = menu_load(variable_get('menu_secondary_links_source', 'user-menu'));
+    
+    // Build links
+    $tree = menu_tree_page_data($secondary_menu['menu_name']);
+    $variables['secondary_menu'] = twitter_bootstrap_menu_navigation_links($tree);
+    
+    // Build list
+    $variables['secondary_nav'] = theme('twitter_bootstrap_btn_dropdown', array(
+      'links' => $variables['secondary_menu'],
+      'label' => $secondary_menu['title'],
+      'type' => 'success',
+      'attributes' => array(
+        'id' => 'user-menu',
+        'class' => array('pull-right'),
+      ),
+    ));
+  }
+  
+  // Replace tabs with dropw down version
+  $variables['tabs']['#primary'] = _twitter_bootstrap_local_tasks($variables['tabs']['#primary']);
+}
+
+
+/**
+ * Preprocess variables for region.tpl.php
+ *
+ * @see region.tpl.php
+ */
+function CMS_theme_preprocess_region(&$variables, $hook) {
+	if ($variables['region'] == 'content') {
+		$variables['theme_hook_suggestions'][] = 'region__no_wrapper';
+	}
+	// Me likes
+	if($variables['region'] == "sidebar_first")
+		unset ($variables['classes_array'][2]); // Remove 'well' coming from twitter_bootstrap
 }
--- cms_theme/templates/page.tpl.php
+++ cms_theme/templates/page.tpl.php
@@ -1,102 +1,85 @@
-<header id="navbar" role="banner" class="navbar navbar-fixed-top">
-  <div class="navbar-inner">
-  	<div class="container">
-  	  <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-  	  <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-  		<span class="icon-bar"></span>
-  		<span class="icon-bar"></span>
-  		<span class="icon-bar"></span>
-  	  </a>
-  	  
-  	  <?php if ($logo): ?>
-    		<a class="brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-    		  <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-    		</a>
-  	  <?php endif; ?>
-
-  	  <?php if ($site_name || $site_slogan): ?>
-    		<hgroup id="site-name-slogan">
-    		  <?php if ($site_name): ?>
-    			<h1>
-    			  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="brand"><?php print $site_name; ?></a>
-    			</h1>
-    		  <?php endif; ?>
-    		</hgroup>
-  	  <?php endif; ?>
-  	  
-  	  <div class="nav-collapse">
-    	  <nav role="navigation">
-      		<?php if ($primary_nav): ?>
-      		  <?php print $primary_nav; ?>
-      		<?php endif; ?>
-      	  
-      		<?php if ($search): ?>
-      		  <?php if ($search): print render($search); endif; ?>
-      		<?php endif; ?>
-      		
-      		<?php if ($secondary_nav): ?>
-      		  <?php print $secondary_nav; ?>
-      		<?php endif; ?>
-    		</nav>
-  	  </div>         
-  	</div>
-  </div>
-</header>
-
+<?php
+// var_dump(variable_get('site_name', 'drupal'));
+// var_dump($site_name);
+?>
 <div class="container">
+	<header id="navbar" role="banner">
+		<div class="row">
+			<div class="span2 logo">
+				<?php if ($logo): ?>
+				<a class="brand" href="#" alt="<?php print t('Home'); ?>" title="<?php print t('Home'); ?>"><img src="http://www.cms.int/images/layout/frame/cms_logo.jpg" width="130" height="79" alt="Click the CMS logo to return to the main entry page"></a>
+				<?php endif; ?>
+			</a>
+			</div>
+			
+			<div class="span10">
+				<ul class="nav-pills">
+					<?php if ($primary_nav): ?>
+						<?php print $primary_nav; ?>
+					<?php endif; ?>
+				</ul>
+				<nav role="navigation">
+				<?php if ($secondary_nav): ?>
+					<?php print $secondary_nav; ?>
+				<?php endif; ?>
+				</nav>
+				<?php if ($site_name || $site_slogan): ?>
+					<hgroup id="site-name-slogan">
+					<?php if ($site_name): ?>
+						<?php print $site_name; ?>
+					<?php endif; ?>
+					</hgroup>
+				<?php endif; ?>
+			</div>
+		</div>
+	</header>
 
-  <header role="banner" id="page-header">
-    <?php if ( $site_slogan ): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-  </header> <!-- /#header -->
-	
 	<div class="row">
-	  
-    <?php if ($page['sidebar_first']): ?>
-      <aside class="span3" role="complementary">
-        <?php print render($page['sidebar_first']); ?>
-      </aside>  <!-- /#sidebar-first -->
-    <?php endif; ?>  
-	  
-	  <section class="<?php print _twitter_bootstrap_content_span($columns); ?>">  
-      <?php if ($page['highlighted']): ?>
-        <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
-      <?php endif; ?>
-      <?php if ($breadcrumb): print $breadcrumb; endif;?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if ($tabs): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if ($page['help']): ?> 
-        <div class="well"><?php print render($page['help']); ?></div>
-      <?php endif; ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-	  </section>
-
-    <?php if ($page['sidebar_second']): ?>
-      <aside class="span3" role="complementary">
-        <?php print render($page['sidebar_second']); ?>
-      </aside>  <!-- /#sidebar-second -->
-    <?php endif; ?>
-
-  </div>
-  <footer class="footer container">
-    <?php print render($page['footer']); ?>
-  </footer>
-</div>
-
-
-	
-
+		<div class="span3">
+			<?php if ($page['sidebar_first']): ?>
+			 <aside class="span3" role="complementary">
+				<?php print render($page['sidebar_first']); ?>
+			 </aside>  <!-- /#sidebar-first -->
+			<?php endif; ?>  
+		</div>
+		<section class="<?php print _twitter_bootstrap_content_span($columns); ?>">  
+			<?php if ($page['highlighted']): ?>
+				<div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
+			<?php endif; ?>
+			<?php if ($breadcrumb): print $breadcrumb; endif;?>
+			<a id="main-content"></a>
+			<?php print render($title_prefix); ?>
+			<?php if ($title): ?>
+				<h1 class="page-header"><?php print $title; ?></h1>
+				<?php endif; ?>
+			<?php print render($title_suffix); ?>
+			<?php print $messages; ?>
+			<?php if ($tabs): ?>
+				<?php print render($tabs); ?>
+			<?php endif; ?>
+			<?php if ($page['help']): ?> 
+				<div class="well"><?php print render($page['help']); ?></div>
+			<?php endif; ?>
+			<?php if ($action_links): ?>
+				<ul class="action-links"><?php print render($action_links); ?></ul>
+			<?php endif; ?>
+			<?php print render($page['content']); ?>
+		</section>
+	</div>
+	<footer class="footer">
+		<div class="row">
+			<div class="span9">
+				<?php print render($page['footer']); ?>
+				<p>&copy; 2004 UNEP / CMS Secretariat, Hermann-Ehlers-Str. 10, 53113 Bonn, Germany</p> 
+				<p>Tel. (+49 228) 815 2401 / 02 &#183;Fax. (+49 228) 815 2449 &#183; E-mail: secretariat@cms.int</p>
+			</div>
+			
+			<div class="span1">
+				<a href="#"><img src="http://www.cms.int/images/layout/unep_logosm.jpg" /></a>
+			</div>
+			<div class="span1">
+				<a href="#"><img src="http://www.cms.int/images/layout/cms_logosm.jpg" /></a>
+			</div>
+		</div>
+	</footer>
+</div><!-- /div.container -->
