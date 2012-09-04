6ecf9fdeef989ea61a615e4b34d0b1f985d47cf0
Piseth Kem <pisethkem@gmail.com>
2012-09-04 11:12:29 +0300

    use __DIR__

--- twitter_bootstrap/theme-settings.php
+++ twitter_bootstrap/theme-settings.php
@@ -1,12 +1,11 @@
 <?php
 
-include_once(dirname(__FILE__) . '/includes/twitter_bootstrap.inc');
+include_once(__DIR__ . '/includes/twitter_bootstrap.inc');
 
 function twitter_bootstrap_form_system_theme_settings_alter(&$form, &$form_state) {
-  $form['theme_settings']['toggle_search'] = array(
-    '#type' => 'checkbox', 
-    '#title' => t('Search box'), 
-    '#default_value' => theme_get_setting('toggle_search'), 
-  );
+    $form['theme_settings']['toggle_search'] = array(
+        '#type' => 'checkbox',
+        '#title' => t('Search box'),
+        '#default_value' => theme_get_setting('toggle_search'),
+    );
 }
-
