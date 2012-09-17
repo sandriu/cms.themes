fff775772a291a09859bf601849a62572ff72ddc
Bogdan Tanase <bogdan.tanase@eaudeweb.ro>
2012-09-17 10:55:10 +0300

    [#35760509] Added Promo region for right static blocks with small banners

--- cms_theme/templates/page.tpl.php
+++ cms_theme/templates/page.tpl.php
@@ -44,33 +44,56 @@
 			 </aside>  <!-- /#sidebar-first -->
 			<?php endif; ?>  
 		</div>
-		<section class="<?php print _twitter_bootstrap_content_span($columns); ?>">  
-			<?php if ($page['highlighted']): ?>
-				<div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
-			<?php endif; ?>
-			<?php if ($breadcrumb): print $breadcrumb; endif;?>
-			<a id="main-content"></a>
-			<?php print render($title_prefix); ?>
-			<?php if ($title): ?>
-				<h1 class="page-header"><?php print $title; ?></h1>
-				<?php endif; ?>
-			<?php print render($title_suffix); ?>
-			<?php print $messages; ?>
-			<?php if ($tabs): ?>
-				<?php print render($tabs); ?>
-			<?php endif; ?>
-			<?php if ($page['help']): ?> 
-				<div class="well"><?php print render($page['help']); ?></div>
-			<?php endif; ?>
-			<?php if ($action_links): ?>
-                            <div class="subnav">
-                                <ul class="nav nav-pills action-links"><?php print render($action_links); ?></ul>
-                            </div>
+                <div class="span9">
+                    <section class="<?php print _twitter_bootstrap_content_span($columns); ?>">  
+                            <?php if ($page['highlighted']): ?>
+                                    <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
+                            <?php endif; ?>
+                            <?php if ($breadcrumb): print $breadcrumb; endif;?>
+                            <a id="main-content"></a>
+                            <?php print render($title_prefix); ?>
+                            <?php if ($title): ?>
+                                    <h1 class="page-header"><?php print $title; ?></h1>
+                                    <?php endif; ?>
+                            <?php print render($title_suffix); ?>
+                            <?php print $messages; ?>
+                            <?php if ($tabs): ?>
+                                    <?php print render($tabs); ?>
+                            <?php endif; ?>
+                            <?php if ($page['help']): ?> 
+                                    <div class="well"><?php print render($page['help']); ?></div>
+                            <?php endif; ?>
+                            <?php if ($action_links): ?>
+                                <div class="subnav">
+                                    <ul class="nav nav-pills action-links"><?php print render($action_links); ?></ul>
+                                </div>
 
-			<?php endif; ?>
-                        <?php print render($page['submenu']); ?>
-			<?php print render($page['content']); ?>
-		</section>
+                            <?php endif; ?>
+                            <?php print render($page['submenu']); ?>
+
+                            <div class="row">
+                                <?php
+                                    if (!empty($page['promo'])) {
+                                ?>
+                                <div class="span6">
+                                <?php print render($page['content']); ?>
+                                </div>
+                                <div class="span3">
+                                <?php print render($page['promo']); ?>
+                                </div>
+                                <?php
+                                    }else {
+                                ?>
+                                <div class="span9">
+                                <?php print render($page['content']); ?>
+                                </div>
+                                <?php
+                                    }
+                                ?>
+                                
+                            </div>
+                    </section>
+                </div>
 	</div>
 	<footer class="footer">
 		<div class="row">
