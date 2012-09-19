358a179931afce21e3e09689396a4211c90060de
Bogdan Tanase <bogdan.tanase@eaudeweb.ro>
2012-09-19 15:29:33 +0300

    [#36232119] Species fields grouped in fieldsets; hook_form_alter() implemented in template.php

--- cms_admin/template.php
+++ cms_admin/template.php
@@ -1,12 +1,8 @@
 <?php
 
 /**
- * Override or insert variables into the html template.
- */
-function cms_admin_preprocess_html(&$vars) {
-  $theme_path = path_to_theme();
-}
-
+ * Implements hook_theme();
+*/
 function cms_admin_theme($existing, $type, $theme, $path) {
     return array(
         'species_node_form' => array(
@@ -16,3 +12,35 @@ function cms_admin_theme($existing, $type, $theme, $path) {
         ),
     );
 }
+
+/**
+ * Implements hook_form_alter();
+*/
+function cms_admin_form_alter(&$form, $form_state, $form_id) {
+    /**
+     * Alter form only for the form IDs listed below
+    */
+    $form_ids = array(
+        'species_node_form',
+    );
+
+    if (isset($form_id) && in_array($form_id, $form_ids)) {
+        /**
+         * Hide additional settings such as Authoring, Comments...
+        */
+        hide($form['additional_settings']);
+
+        /**
+         * Add Back button
+        */
+        $form['actions']['cancel'] = array(
+            '#type' => 'button',
+            '#value' => t('Back'),
+            '#access' => TRUE,
+            '#weight' => 15,
+            '#attributes' => array('onClick' => 'history.go(-1); return true;'),
+            '#post_render' => array('_change_submit_to_button'),
+        );
+    }
+}
+
