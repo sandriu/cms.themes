304a7183ca7e23838bc3aa76fb9efb4a0485f95d
Bogdan Tanase <bogdan.tanase@eaudeweb.ro>
2012-09-20 17:09:53 +0300

    [#36141351] WIP: Cleanup public interface

--- cms_theme/templates/page.tpl.php
+++ cms_theme/templates/page.tpl.php
@@ -1,103 +1,77 @@
-<?php
-// var_dump(variable_get('site_name', 'drupal'));
-// var_dump($site_name);
-?>
+<div class="navbar navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+
+            <a class="brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+                <?php
+                    print $site_name;
+                ?>
+            </a>
+        </div>
+    </div>
+</div>
+
 <div class="container">
-	<header id="navbar" role="banner">
-		<div class="row">
-			<div class="span2 logo">
-				<?php if ($logo): ?>
-				<a class="brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-                                    <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-                                </a>
-				<?php endif; ?>
-			</a>
-			</div>
-			
-			<div class="span10">
-				<ul class="nav-pills">
-					<?php if ($primary_nav): ?>
-						<?php print $primary_nav; ?>
-					<?php endif; ?>
-				</ul>
-				<nav role="navigation">
-				<?php if ($secondary_nav): ?>
-					<?php print $secondary_nav; ?>
-				<?php endif; ?>
-				</nav>
-				<?php if ($site_name || $site_slogan): ?>
-					<hgroup id="site-name-slogan">
-					<?php if ($site_name): ?>
-						<?php print $site_name; ?>
-					<?php endif; ?>
-					</hgroup>
-				<?php endif; ?>
-			</div>
-		</div>
-	</header>
+    <div class="row">
+        <div class="span12">
+            <?php
+                if ($breadcrumb) {
+                    #print $breadcrumb;
+                }
+            ?>
 
-	<div class="row">
-		<div class="span3">
-			<?php if ($page['sidebar_first']): ?>
-			 <aside class="span3" role="complementary">
-				<?php print render($page['sidebar_first']); ?>
-			 </aside>  <!-- /#sidebar-first -->
-			<?php endif; ?>  
-		</div>
-                <div class="span9">
-                    <section class="<?php print _twitter_bootstrap_content_span($columns); ?>">  
-                            <?php if ($page['highlighted']): ?>
-                                    <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
-                            <?php endif; ?>
-                            <?php if ($breadcrumb): print $breadcrumb; endif;?>
-                            <a id="main-content"></a>
-                            <?php print render($title_prefix); ?>
-                            <?php if ($title): ?>
-                                    <h1 class="page-header"><?php print $title; ?></h1>
-                                    <?php endif; ?>
-                            <?php print render($title_suffix); ?>
-                            <?php print $messages; ?>
-                            <?php if ($tabs): ?>
-                                    <?php print render($tabs); ?>
-                            <?php endif; ?>
-                            <?php if ($page['help']): ?> 
-                                    <div class="well"><?php print render($page['help']); ?></div>
-                            <?php endif; ?>
-                            <?php if ($action_links): ?>
-                                <div class="subnav">
-                                    <ul class="nav nav-pills action-links"><?php print render($action_links); ?></ul>
-                                </div>
+            <?php if ($title): ?>
+            <h1>
+            <?php print $title; ?>
+            </h1>
+            <?php endif; ?>
 
-                            <?php endif; ?>
-                            <?php print render($page['submenu']); ?>
+            <?php print $messages; ?>
 
-                            <div class="row">
-                                <?php
-                                    if (!empty($page['promo'])) {
-                                ?>
-                                <div class="span6">
-                                <?php print render($page['content']); ?>
-                                </div>
-                                <div class="span3">
-                                <?php print render($page['promo']); ?>
-                                </div>
-                                <?php
-                                    }else {
-                                ?>
-                                <div class="span9">
-                                <?php print render($page['content']); ?>
-                                </div>
-                                <?php
-                                    }
-                                ?>
-                                
-                            </div>
-                    </section>
+            <?php
+            $match = FALSE;
+            $types = array('species');
+            if (arg(0) == 'node' && is_numeric(arg(1))) {
+                $nid = arg(1);
+                $node = node_load($nid);
+                $type = $node->type;
+                if (in_array($type, $types)) {
+                    $match = TRUE;
+                }
+            }
+
+            if ($tabs and !$match) {
+                print render($tabs);
+            }
+            ?>
+
+            <?php if ($page['help']): ?> 
+            <div class="well">
+                <?php print render($page['help']); ?>
+            </div>
+            <?php endif; ?>
+
+            <?php if ($action_links and !$match): ?>
+                <div class="subnav">
+                    <ul class="nav nav-pills action-links"><?php print render($action_links); ?></ul>
                 </div>
-	</div>
-	<footer class="footer">
-		<div class="row">
-			<?php print render($page['footer']); ?>
-		</div>
-	</footer>
+
+            <?php endif; ?>
+
+            <?php print render($page['submenu']); ?>
+
+            <?php print render($page['content']); ?>
+        </div>
+    </div>
+
+    <footer class="footer">
+        <div class="row">
+        <?php print render($page['footer']); ?>
+        </div>
+    </footer>
 </div><!-- /div.container -->
