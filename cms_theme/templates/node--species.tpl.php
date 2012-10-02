f5ad7c5d724944ac31b548265de944d42f02dd82
Bogdan Tanase <bogdan.tanase@eaudeweb.ro>
2012-10-02 16:08:57 +0300

    [#37024199] Added Twitter Bootstrap Carousel

--- cms_theme/templates/node--species.tpl.php
+++ cms_theme/templates/node--species.tpl.php
@@ -60,10 +60,44 @@
                     <?php echo render($content['field_species_subspecies']); ?>
                 </table>
             </div>
+            
             <div class="span5">
-                <div class="gallery">
-                    PICTURE HERE
+                <?php
+                    if (count($node->gallery)) {
+                ?>
+                <div id="myCarousel" class="carousel slide" style="height: 148px; overflow: hidden; text-align: center;">
+                    <!-- Carousel items -->
+                    <div class="carousel-inner">
+                        <div class="active item">
+                            <?php echo $node->gallery[0]; unset($node->gallery[0]); ?>
+                        </div>
+                        <?php
+                            foreach ($node->gallery as $index => $image) {
+                        ?>
+                            <div class="item">
+                                <?php
+                                    echo $image;
+                                ?>
+                            </div>
+                        <?php
+                            }
+                        ?>
+                    </div>
+                    <!-- Carousel nav -->
+                    <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                    <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+                  </div>
+                <?php
+                    } else {
+                ?>
+                <div class="alert alert-info">
+                    <p>
+                        No pictures for <?php echo $node->title; ?>
+                    </p>
                 </div>
+                <?php
+                    }
+                ?>
             </div>
         </div>
 
