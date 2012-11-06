9ef72413c1a7502132fd73ee7aedf65bca5a57aa
Cristian Romanescu <cristian@romanescu.ro>
2012-11-06 12:03:04 +0200

    show default reset button

--- cms_theme/templates/views-exposed-form--parties_listing.tpl.php
+++ cms_theme/templates/views-exposed-form--parties_listing.tpl.php
@@ -74,7 +74,7 @@
 
     <?php if (!empty($reset_button)): ?>
       <div class="views-exposed-widget views-reset-button">
-        <a class="reset-link" id="edit-reset" name="op" onclick="jQuery('#views-exposed-form-parties-list-parties-listing').resetForm();">Clear filters</a>
+          <?php print $reset_button; ?>
       </div>
     <?php endif; ?>
   </div>
