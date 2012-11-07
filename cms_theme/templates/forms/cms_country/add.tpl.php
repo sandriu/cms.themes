55274478a883e8a37ba333a695635cfb783d9b2f
Bogdan Tanase <bogdan.tanase@eaudeweb.ro>
2012-11-07 18:06:35 +0200

    Updated Twitter Boostrap to 2.2.1

--- cms_theme/templates/forms/cms_country/add.tpl.php
+++ cms_theme/templates/forms/cms_country/add.tpl.php
@@ -1,27 +1,30 @@
 <div class="row">
     <div class="span12">
-        <div class="span6 no-left-margin">
+        <div class="span4 no-left-margin">
             <?php
                 echo drupal_render($form['field_cms_country']);
             ?>
         </div>
-        <div class="span6 no-left-margin">
+
+        <div class="span4">
             <?php
                 echo drupal_render($form['field_cms_country_region']);
             ?>
         </div>
     </div>
-
     <div class="span12">
-        <div class="span6 no-left-margin">
-            <?php
-                echo drupal_render($form['field_cms_country_party_no']);
-            ?>
-        </div>
-        <div class="span6 no-left-margin">
+        <div class="span5 no-left-margin">
+            <div class="pull-left">
             <?php
                 echo drupal_render($form['field_cms_country_entry_force']);
             ?>
+            </div>
+
+            <div class="pull-left span2">
+            <?php
+                echo drupal_render($form['field_cms_country_party_no']);
+            ?>
+            </div>
         </div>
     </div>
 
