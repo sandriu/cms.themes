784f406731bd3ad3e798a4a94876c02b10840e63
Bogdan Tanase <bogdan.tanase@eaudeweb.ro>
2013-01-17 17:34:57 +0200

    Show add button for all content types

--- cms_theme/template.php
+++ cms_theme/template.php
@@ -173,7 +173,7 @@ function check_display_field($object) {
 }
 
 function get_cms_types() {
-    return array('species', 'cms_party', 'publication', 'legal_instrument', 'countries', 'cms_country', 'listing');
+    return array('species', 'cms_party', 'publication', 'legal_instrument', 'countries', 'cms_country', 'listing', 'decision', 'meeting', 'national_plan', 'national_report');
 }
 
 function show_add_button() {
