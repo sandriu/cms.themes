91185dd7bca3f83444e01f8ac002c191b2732d29
Bogdan Tanase <bogdan.tanase@eaudeweb.ro>
2013-01-18 15:23:09 +0200

    [#42786819] Merged uploaded images with Arkive images for Species items

--- cms_theme/templates/slots/species/gallery.tpl.php
+++ cms_theme/templates/slots/species/gallery.tpl.php
@@ -1,38 +1,35 @@
 <div class="span5">
     <?php
-        if (isset($node->field_species_images) && (!empty($node->field_species_images))) {
+        if ((isset($node->field_species_images) && (!empty($node->field_species_images))) || count($node->gallery)) {
     ?>
     <div id="myCarousel" class="carousel slide img-polaroid species-carousel">
         <!-- Carousel items -->
         <div class="carousel-inner">
             <div class="active item">
+                <?php
+                    if (isset($node->field_species_images) && (!empty($node->field_species_images))) {
+                ?>
                 <img src="<?php echo file_create_url($node->field_species_images[$node->language][0]['uri']); ?>" alt="" title="" class="species-custom-image" />
+                <?php
+                    unset($node->field_species_images[$node->language][0]);
+                    }elseif (count($node->gallery)) {
+                        echo $node->gallery[0];
+                        unset($node->gallery[0]);
+                    }
+                ?>
             </div>
         <?php
-            unset($node->field_species_images[$node->language][0]);
-            foreach ($node->field_species_images[$node->language] as $index => $image) {
+            if (isset($node->field_species_images) && (!empty($node->field_species_images))) {
+                foreach ($node->field_species_images[$node->language] as $index => $image) {
         ?>
             <div class="item">
                 <img src="<?php echo file_create_url($image['uri']); ?>" alt="" title="" class="species-custom-image" />
             </div>
         <?php
+                }
             }
-        ?>
-        </div>
 
-        <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-        <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
-    </div>
-    <?php
-        } elseif (count($node->gallery)) {
-    ?>
-    <div id="myCarousel" class="carousel slide img-polaroid species-carousel">
-        <!-- Carousel items -->
-        <div class="carousel-inner">
-            <div class="active item">
-                <?php echo $node->gallery[0]; unset($node->gallery[0]); ?>
-            </div>
-            <?php
+            if (count($node->gallery)){
                 foreach ($node->gallery as $index => $image) {
             ?>
                 <div class="item">
@@ -42,14 +39,15 @@
                 </div>
             <?php
                 }
-            ?>
+            }
+        ?>
         </div>
-        <!-- Carousel nav -->
+
         <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
         <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
     </div>
     <?php
-        } else {
+        }else {
     ?>
     <div class="alert alert-info species-alert">
         <p>
