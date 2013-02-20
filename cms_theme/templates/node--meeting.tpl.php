d67942c3218189e2e949aa07c8782ba62568c8ea
Bogdan Tanase <bogdan.tanase@eaudeweb.ro>
2013-02-20 17:42:54 +0200

    [#43644071] Updated to Twitter Bootstrap 2.3.0; List Meetings documents

--- cms_theme/templates/node--meeting.tpl.php
+++ cms_theme/templates/node--meeting.tpl.php
@@ -18,6 +18,7 @@
                 echo render($content['field_meeting_access']);
                 echo render($content['field_meeting_status']);
                 echo render($content['field_meeting_languages']);
+                
             ?>
             </tbody>
         </table>
@@ -43,6 +44,34 @@
         </div>
         
     </div>
+
+    <?php
+        if (isset($content['field_meeting_documents'])) {
+            $total_documents = count($content['field_meeting_documents']['#items']);
+    ?>
+    <div class="span12">
+        <div class="accordion" id="accordion2">
+            <div class="accordion-group">
+                <div class="accordion-heading">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+                        <?php
+                            echo t('Documents') . " (" . $total_documents .")";
+                        ?>
+                    </a>
+                </div>
+                <div id="collapseOne" class="accordion-body collapse in">
+                    <div class="accordion-inner">
+                        <?php
+                            echo render($content['field_meeting_documents']);
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+        }
+    ?>
 </div>
 
 <?php
