3b55e3b082bb93f749257937ff53f8ce99f9a9b0
Bogdan Tanase <bogdan.tanase@eaudeweb.ro>
2013-04-03 18:18:37 +0300

    [#47268341] Show Meeting related content as tabs

--- cms_theme/templates/node--meeting.tpl.php
+++ cms_theme/templates/node--meeting.tpl.php
@@ -45,33 +45,10 @@
         
     </div>
 
-    <?php
-        if (isset($content['field_meeting_documents'])) {
-            $total_documents = count($content['field_meeting_documents']['#items']);
-    ?>
     <div class="span12">
-        <div class="accordion" id="accordion2">
-            <div class="accordion-group">
-                <div class="accordion-heading">
-                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
-                        <?php
-                            echo t('Documents') . " (" . $total_documents .")";
-                        ?>
-                    </a>
-                </div>
-                <div id="collapseOne" class="accordion-body collapse in">
-                    <div class="accordion-inner">
-                        <?php
-                            echo render($content['field_meeting_documents']);
-                        ?>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?php echo render_slot($node, 'related-content', 'meeting', $content); ?>
     </div>
-    <?php
-        }
-    ?>
+
 </div>
 
 <?php
