f340b82fcf20d31f991b01f5459c21e2bcce0017
Bogdan Tănase <bogdan.tanase@eaudeweb.ro>
2013-07-08 18:12:46 +0300

    Updated CMS Instruments item view

--- cms_theme/templates/node--legal_instrument.tpl.php
+++ cms_theme/templates/node--legal_instrument.tpl.php
@@ -2,10 +2,8 @@
     render_slot($node, 'node-buttons', 'general');
 
     render_slot($node, 'details', 'legal_instrument', $content);
-    render_slot($node, 'dates', 'legal_instrument', $content);
-    render_slot($node, 'description', 'legal_instrument', $content);
-    render_slot($node, 'coverage', 'legal_instrument', $content);
     render_slot($node, 'attachments', 'legal_instrument', $content);
+
 
     hide($content['links']);
     hide($content['comments']);
--- cms_theme/templates/slots/legal_instrument/details.tpl.php
+++ cms_theme/templates/slots/legal_instrument/details.tpl.php
@@ -11,10 +11,29 @@
     <tbody>
         <?php echo render($content['field_instrument_name']); ?>
         <?php echo render($content['field_instrument_type']); ?>
-        <?php echo render($content['field_instrument_host_country']); ?>
-        <?php echo render($content['field_instrument_depository']); ?>
-        <?php echo render($content['field_instrument_secretariat']); ?>
+        <?php echo render($content['field_instrument_status']); ?>
+        <?php echo render($content['field_instrument_languages']); ?>
         <?php echo render($content['field_instrument_sponsor']); ?>
+        <?php echo render($content['field_instrument_depository']); ?>
+        <?php echo render($content['field_instrument_coverage']); ?>
+        <?php echo render($content['field_instrument_range']); ?>
+        <?php echo render($content['field_instrument_final_act']); ?>
+        <?php echo render($content['field_instrument_signature']); ?>
+        <?php echo render($content['field_instrument_host_country']); ?>
+        <?php echo render($content['field_instrument_in_effect']); ?>
+        <?php echo render($content['field_instrument_in_force']); ?>
+        <?php echo render($content['field_instrument_actual_effect']); ?>
+        <?php echo render($content['field_instrument_actual_force']); ?>
+        <?php echo render($content['field_instrument_secretariat']); ?>
+        <?php echo render($content['field_instrument_financing']); ?>
+        <?php echo render($content['field_instrument_reservations']); ?>
+        <?php echo render($content['field_instrument_description']); ?>
+        <?php echo render($content['field_instrument_species_ex']); ?>
+        <?php echo render($content['field_instrument_other']); ?>
+        <?php echo render($content['field_instrument_international_init']); ?>
+        <?php echo render($content['field_working_groups']); ?>
+        <?php echo render($content['field_member_of_big']); ?>
+        <?php echo render($content['field_instrument_amendments']); ?>
     </tbody>
 </table>
 <?php
