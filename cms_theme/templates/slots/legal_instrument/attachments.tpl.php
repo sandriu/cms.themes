15ebf942281777e3c70fd47986637921d4591e79
Bogdan Tănase <bogdan.tanase@eaudeweb.ro>
2013-07-16 12:32:09 +0300

    Render CMS Instrument Signed by field

--- cms_theme/templates/slots/legal_instrument/attachments.tpl.php
+++ cms_theme/templates/slots/legal_instrument/attachments.tpl.php
@@ -1,7 +1,14 @@
 <?php
     if (check_display_field($content, 'field_instrument_treaty_text')) {
 ?>
+
+<div class="clear">&nbsp;</div>
 <hr />
+
+<h4>
+    <?php echo t('Treaty text'); ?>
+</h4>
+
 <?php
     echo render($content['field_instrument_treaty_text']);
 ?>
--- cms_theme/templates/slots/legal_instrument/details.tpl.php
+++ cms_theme/templates/slots/legal_instrument/details.tpl.php
@@ -40,4 +40,39 @@
     }
 ?>
 
+<hr />
+
+<?php
+    if(check_display_field($content, 'field_instrument_signed_by')) {
+?>
+<h4>
+    <?php echo t('Signed by'); ?>
+</h4>
+<br />
+
+<table class="table table-condensed table-hover table-bordered table-striped dataTable">
+
+    <thead>
+        <tr>
+            <th><?php echo t('Country'); ?></th>
+            <th><?php echo t('Date'); ?></th>
+            <th><?php echo t('Status'); ?></th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php echo render($content['field_instrument_signed_by']); ?>
+    </tbody>
+</table>
+<?php
+    }
+?>
+
+<div class="clear">&nbsp;</div>
+<hr />
+
+<h4>
+    <?php echo t('Countries'); ?>
+</h4>
+<br />
+
 <?php echo render($content['countries']); ?>
