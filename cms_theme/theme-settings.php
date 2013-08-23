07d2820f14fd63b217b9cd5d37ef3fae41ee7aad
Bogdan Tănase <bogdan.tanase@eaudeweb.ro>
2013-08-23 17:06:27 +0300

    CMS Theme fully integrated with Twitter Bootstrap theme.

--- cms_theme/theme-settings.php
+++ cms_theme/theme-settings.php
@@ -1,8 +1,8 @@
 <?php
 
-include_once(__DIR__ . '/includes/twitter_bootstrap.inc');
+include_once(__DIR__ . '/includes/cms_theme.inc');
 
-function twitter_bootstrap_form_system_theme_settings_alter(&$form, &$form_state) {
+function cms_theme_form_system_theme_settings_alter(&$form, &$form_state) {
     $form['theme_settings']['toggle_search'] = array(
         '#type' => 'checkbox',
         '#title' => t('Search box'),
