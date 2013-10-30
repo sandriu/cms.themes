725278f5f5656d95aa6b982b16c566b280a633ec
Bogdan Tănase <bogdan.tanase@eaudeweb.ro>
2013-10-30 12:49:52 +0200

    jQuery should be loaded using jquery_update

--- cms_theme/template.php
+++ cms_theme/template.php
@@ -87,7 +87,7 @@ function CMS_theme_theme() {
 }
 
 function cms_theme_js_alter(&$javascript) {
-    $javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'cms_theme').'/js/jquery-1.8.0.min.js';
+    #$javascript['misc/jquery.js']['data'] = drupal_get_path('theme', 'cms_theme').'/js/jquery-1.8.0.min.js';
     if(array_key_exists('misc/jquery.form.js', $javascript)) {
         $javascript['misc/jquery.form.js']['data'] = drupal_get_path('theme', 'cms_theme').'/js/jquery.form.js';
     }
