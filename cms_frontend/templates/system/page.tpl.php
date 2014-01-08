455d2743f3b4b41203290203d1994f08eff68455
Sonia Andriu <sonia.andriu@eaudeweb.ro>
2014-01-08 16:12:46 +0100

    refactor the header and navigation menus; remove unnecessary images

--- cms_frontend/templates/system/page.tpl.php
+++ cms_frontend/templates/system/page.tpl.php
@@ -4,15 +4,9 @@
   <div class="container">
       <div class="row">   
       
-        <div class="left-header col-md-7">     
+        <div class="left-header col-md-1">     
 
-          <div class="top-user-menu">        
-            <?php 
-            $addblock = module_invoke('locale','block_view','language_content');
-            print render($addblock['content']);
-            print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('class' => array('user-menu')), 'heading' => array('text' => t('Secondary menu'),'level' => 'h2','class' => array('element-invisible')))); 
-            ?>    
-          </div>    
+  
 
           <div class="site-identity navbar-header">
             <?php if ($logo): ?>
@@ -20,11 +14,7 @@
               <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
             </a>
             <?php endif; ?>  
-                          
-            <?php if (!empty($site_slogan)): ?>
-              <h1 class="portal-title navbar-brand"><?php print $site_slogan; ?></h1>
-            <?php endif; ?>
-                          
+       
             <!--.btn-navbar is used as the toggle for collapsed navbar content -->
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
               <span class="sr-only">Toggle navigation</span>
@@ -36,13 +26,32 @@
 
         </div>   
 
-        <div class="right-header col-md-5">
-          <?php print render($page['header']); ?>
-          
-          <div class="small-logos">
-            <a href="http://www.unep.org/" class="UNEP-logo"><?php print theme('image', array('path'=>drupal_get_path('theme', 'cms_frontend').'/images/UNEP_white_logo_32x34.png', 'alt'=>'UNEP logo')); ?></a>
-            <a href="http://www.cms.int/" class="CMS-logo-small"><?php print theme('image', array('path'=>drupal_get_path('theme', 'cms_frontend').'/images/CMS_white_logo_32x34.png', 'alt'=>'CMS logo')); ?></a>
+        <div class="right-header col-md-11">
+        
+          <div class="up-area">
+            <div class="top-user-menu">        
+              <?php 
+              $addblock = module_invoke('locale','block_view','language_content');
+              print render($addblock['content']);
+              print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('class' => array('user-menu')), 'heading' => array('text' => t('Secondary menu'),'level' => 'h2','class' => array('element-invisible')))); 
+              ?>    
+            </div>  
+            
+            <div class="search-area">
+              <?php print render($page['header']); ?>
+              <div class="small-logos">
+                <a href="http://www.unep.org/" class="UNEP-logo"><?php print theme('image', array('path'=>drupal_get_path('theme', 'cms_frontend').'/images/UNEP_white_logo_32x34.png', 'alt'=>'UNEP logo')); ?></a>
+                <a href="http://www.cms.int/" class="CMS-logo-small"><?php print theme('image', array('path'=>drupal_get_path('theme', 'cms_frontend').'/images/CMS_white_logo_32x34.png', 'alt'=>'CMS logo')); ?></a>
+              </div>
+            </div>
           </div>
+          
+          <div class="down-area">
+            <?php if (!empty($site_slogan)): ?>
+              <h1 class="portal-title navbar-brand"><?php print $site_slogan; ?></h1>
+            <?php endif; ?>
+          </div>
+
         </div>
       
 
@@ -92,7 +101,7 @@
     </aside-->
     
     <!-- center column --> 
-    <div class="region col-md-9 center-column">            
+    <div class="region col-md-8 center-column">            
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
@@ -119,7 +128,7 @@
     
     <!-- right column-->
     <?php if (!empty($page['sidebar_second'])): ?>
-    <aside class="col-md-3 right-column" role="complementary">
+    <aside class="col-md-4 right-column" role="complementary">
       <div class="well region region-sidebar-second">                
         <?php print render($page['sidebar_second']); ?>        
       </div>
