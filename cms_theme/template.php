85260ce7089735a8b93d754be2d30c3070b21a2a
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2014-01-20 17:13:44 +0200

    Added hook from boostrap theme to cms_theme to add form-item class - solves edit/hide summary bug on front-end

--- cms_theme/template.php
+++ cms_theme/template.php
@@ -223,6 +223,92 @@ function cms_theme_element_info_alter(&$type) {
     }
 }
 
+
+/**
+ * Function copied from twitter_bootstrap theme - includes/modules/form.inc
+ * Added 'form-item' class to fields to solve front-end bug - not showing edit summary button on body field
+ *    - the js that handles the button is using 'form-item' class to position the edit/hide summary link
+ *    - the class needs to be on container div of summary and also on container div of body
+ *
+ * @param $variables
+ *
+ * @return string
+ */
+
+function cms_theme_form_element(&$variables) {
+    $element = &$variables['element'];
+    // This is also used in the installer, pre-database setup.
+    $t = get_t();
+
+    // This function is invoked as theme wrapper, but the rendered form element
+    // may not necessarily have been processed by form_builder().
+    $element += array(
+        '#title_display' => 'before',
+    );
+
+    // Add element #id for #type 'item'.
+    if (isset($element['#markup']) && !empty($element['#id'])) {
+        $attributes['id'] = $element['#id'];
+    }
+
+    // Add bootstrap class
+    $attributes['class'] = array('control-group');
+
+    // Check for errors and set correct error class
+    if (isset($element['#parents']) && form_get_error($element)) {
+        $attributes['class'][] = 'error';
+    }
+
+    if (!empty($element['#type'])) {
+        $attributes['class'][] = 'form-type-' . strtr($element['#type'], '_', '-');
+    }
+    if (!empty($element['#name'])) {
+        $attributes['class'][] = 'form-item form-item-' . strtr($element['#name'], array(' ' => '-', '_' => '-', '[' => '-', ']' => ''));
+    }
+    // Add a class for disabled elements to facilitate cross-browser styling.
+    if (!empty($element['#attributes']['disabled'])) {
+        $attributes['class'][] = 'form-disabled';
+    }
+    $output = '<div' . drupal_attributes($attributes) . '>' . "\n";
+
+    // If #title is not set, we don't display any label or required marker.
+    if (!isset($element['#title'])) {
+        $element['#title_display'] = 'none';
+    }
+    $prefix = isset($element['#field_prefix']) ? '<span class="field-prefix">' . $element['#field_prefix'] . '</span> ' : '';
+    $suffix = isset($element['#field_suffix']) ? ' <span class="field-suffix">' . $element['#field_suffix'] . '</span>' : '';
+
+    switch ($element['#title_display']) {
+        case 'before':
+        case 'invisible':
+            $output .= ' ' . theme('form_element_label', $variables);
+            $output .= '<div class="controls">';
+            $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+            break;
+
+        case 'after':
+            $output .= '<div class="controls">';
+            $variables['#children'] = ' ' . $prefix . $element['#children'] . $suffix;
+            $output .= ' ' . theme('form_element_label', $variables) . "\n";
+            break;
+
+        case 'none':
+        case 'attribute':
+            // Output no label and no required marker, only the children.
+            $output .= '<div class="controls">';
+            $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+            break;
+    }
+
+    if ( !empty($element['#description']) ) {
+        $output .= '<p class="help-block">' . $element['#description'] . "</p>\n";
+    }
+
+    $output .= "</div></div>\n";
+
+    return $output;
+}
+
 /**
  * Hide Text formats option from Text with summary fields
 */
