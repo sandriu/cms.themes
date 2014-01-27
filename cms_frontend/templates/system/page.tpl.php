bba494e41b4a326b9e6aabb821cfcd9b02f8a27a
rciofringeanu <radu.sc@gmail.com>
2014-01-27 16:51:50 +0200

    Add footer new region

--- cms_frontend/templates/system/page.tpl.php
+++ cms_frontend/templates/system/page.tpl.php
@@ -141,6 +141,15 @@
 <footer>
   <div class="well-footer subscribe-social">
     <div class="container">
+      <div class="row">        
+        <div class="col-md-12">
+          <?php print render($page['footer_menu']); ?>           
+        </div>
+      </div>
+    </div>
+  </div>  
+  <div class="well-footer subscribe-social">
+    <div class="container">
       <div class="row">
         <div class="col-md-4">
           <?php print render($page['footer_first_row_left']); ?>          
