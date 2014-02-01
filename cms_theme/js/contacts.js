706ef6a71407d5d68038b9b35b3c0dec8d9f0a4f
Cristian Romanescu <cristian@romanescu.ro>
2014-02-01 18:35:11 +0200

    refs #1883 - Moved datatables to server side paging via ajax.

--- cms_theme/js/contacts.js
+++ cms_theme/js/contacts.js
@@ -1,7 +1,6 @@
 (function($) {
     $(document).ready(function() {
         $('a[rel="tooltip"]').tooltip();
-
         var per_page = $('#contacts-per-page').val();
         var page = $('#current-page').val();
         var operator = $('#operator').val();
@@ -16,61 +15,71 @@
         var species = $('#species').val();
         var meeting = $('#meeting').val();
 
-        $('#contacts-listing').dataTable({
-            "bProcessing": false,
-            "bServerSide": false,
+        var dt = $('#contacts-listing').dataTable({
+            "bProcessing": true,
+            "bServerSide": true,
+            "sAjaxSource": "/manage/contacts/ajax/list",
+            "bDeferRender": true,
             "bFilter": true,
             "sPaginationType": "bootstrap",
             "aaSorting": [ [1, "asc"] ],
             "aoColumns": [
-                {
-                    "bSearchable": false,
-                    "bVisible": false,
-                    "sDefaultContent": ""
-                },
-                {
-                    "bSearchable": true,
-                    "sDefaultContent": "",
-//                    "fnRender": function (oObj) {
-//                        return "<a href='/" + administration_path + "contacts/item/" + oObj.aData[0] + "/" + oObj.aData[10].toLowerCase() + "/view'>" + oObj.aData[1] + "</a>";
-//                    }
-                },
-                {
-                    "bSearchable": true,
-                    "bVisible": true,
-                    "sDefaultContent": ""
-                },
-                {
-                    "sDefaultContent": ""
-                },
-                {
-                    "bSearchable": true,
-                    "bVisible": false,
-                    "sDefaultContent": ""
-                },
-                {
-                    "bVisible": false,
-                    "sDefaultContent": ""
-                },
-                {
-                    "sDefaultContent": ""
-                },
-                {
-                    "bSearchable": true,
-                    "bVisible": true,
-                    "sDefaultContent": ""
-                },
-                {
-                    "bSearchable": true,
-                    "bVisible": false,
-                    "sDefaultContent": ""
-                },
-                {
-                    "bSearchable": true,
-                    "sDefaultContent": ""
+                { "bVisible": true, "sDefaultContent": "" },
+                { "bVisible": true, "sDefaultContent": "" },
+                { "bVisible": true, "sDefaultContent": "" },
+                { "bVisible": true, "sDefaultContent": "" },
+                { "bVisible": true, "sDefaultContent": "" },
+                { "bVisible": true, "sDefaultContent": "" },
+                { "bVisible": true, "sDefaultContent": "", "bSortable": false }
+            ],
+            "fnServerParams": function (aoData) {
+                var countries = jQuery('#country').val();
+                if(countries !== null) {
+                    jQuery(countries).each(function(i, d) {
+                        aoData.push({ 'name' : 'country[]', 'value' : d});
+                    });
                 }
-            ]
+                var region = jQuery('#region').val();
+                if(region !== null) {
+                    jQuery(region).each(function(i, d) {
+                        aoData.push({ 'name' : 'region[]', 'value' : d});
+                    });
+                }
+                var person_status = jQuery('#person_status').val();
+                if(person_status !== null) {
+                    jQuery(person_status).each(function(i, d) {
+                        aoData.push({ 'name' : 'person_status[]', 'value' : d});
+                    });
+                }
+                var organization_status = jQuery('#organization_status').val();
+                if(organization_status !== null) {
+                    jQuery(organization_status).each(function(i, d) {
+                        aoData.push({ 'name' : 'organization_status[]', 'value' : d});
+                    });
+                }
+                var availability = jQuery('#availability').val();
+                if(availability == 'TRUE' || availability == 'FALSE') {
+                    aoData.push({ 'name' : 'availability', 'value' : availability});
+                }
+                var instrument = jQuery('#instrument').val();
+                if(instrument !== null) {
+                    jQuery(instrument).each(function(i, d) {
+                        aoData.push({ 'name' : 'instrument[]', 'value' : d});
+                    });
+                }
+                aoData.push({ 'name' : 'per_field_operator', 'value' : jQuery('#per_field_operator').val()});
+                aoData.push({ 'name' : 'operator', 'value' : jQuery('#operator').val()});
+            }
         });
+
+        // Delay search after 3 characters
+        $('.dataTables_filter input')
+            .unbind('keypress keyup')
+            .bind('keypress keyup', function(e){
+                if ($(this).val().length < 3 && e.keyCode != 13) return;
+                dt.fnFilter($(this).val());
+            });
+
 
         $('#export-button').click(function(e){
             e.preventDefault();
@@ -91,7 +100,7 @@
         });
 
         $('.accordion-toggle').each(function(){
-            toggle = $(this);
+            var toggle = $(this);
             if (toggle.data('toggle') == 'collapse') {
                 holder = toggle.attr('href');
                 $(holder).css('overflow', 'visible');
