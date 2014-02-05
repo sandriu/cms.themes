26a76dc6ef80ffbfa5100d7bb43819520b12afb4
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2014-02-05 13:01:21 +0200

    Removed forgoten dummy text

--- cms_theme/templates/forms/working_group/add.tpl.php
+++ cms_theme/templates/forms/working_group/add.tpl.php
@@ -1,6 +1,4 @@
-<p>mereg</p>
 <?php
-    //unset($form['#theme']);
     if (arg(0) == 'node' && is_numeric(arg(1))) {
         $node_id = arg(1);
     }
@@ -9,5 +7,4 @@
 <h3>Reorder related documents</h3>
 <?php
     print views_embed_view('working_group_documents_reorder','wg_doc_reorder', $node_id);
-
 ?>
