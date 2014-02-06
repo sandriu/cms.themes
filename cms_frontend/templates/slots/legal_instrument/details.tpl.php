b4b54f8abe897770a4fd6e51b03e1842f073adc7
Sonia Andriu <sonia.andriu@eaudeweb.ro>
2014-02-06 17:13:32 +0100

    Refs #1992: arrange instrument profile into columns

--- cms_frontend/templates/slots/legal_instrument/details.tpl.php
+++ cms_frontend/templates/slots/legal_instrument/details.tpl.php
@@ -6,42 +6,11 @@
         check_display_field($content, 'field_instrument_secretariat') ||
         check_display_field($content, 'field_instrument_sponsor')) {
 ?>
-<table class="table table-condensed table-hover two-columns">
-    <caption><?php echo t('Details'); ?></caption>
-    <tbody>
-        <?php echo render($content['field_instrument_name']); ?>
-        <?php echo render($content['field_instrument_type']); ?>
-        <?php echo render($content['field_instrument_status']); ?>
-        <?php echo render($content['field_languages']); ?>
-        <?php echo render($content['field_instrument_sponsor']); ?>
-        <?php echo render($content['field_instrument_depositary']); ?>
-        <?php echo render($content['field_instrument_coverage']); ?>
-        <?php echo render($content['field_instrument_final_act']); ?>
-        <?php echo render($content['field_instrument_signature']); ?>
-        <?php echo render($content['field_instrument_host_country']); ?>
-        <?php echo render($content['field_instrument_in_effect']); ?>
-        <?php echo render($content['field_instrument_in_force']); ?>
-        <?php echo render($content['field_instrument_actual_effect']); ?>
-        <?php echo render($content['field_instrument_actual_force']); ?>
-        <?php echo render($content['field_instrument_secretariat']); ?>
-        <?php echo render($content['field_instrument_financing']); ?>
-        <?php echo render($content['field_instrument_reservations']); ?>
-        <?php echo render($content['field_instrument_description']); ?>
-        <?php echo render($content['field_instrument_species_ex']); ?>
-        <?php echo render($content['field_instrument_other']); ?>
-        <?php echo render($content['field_instrument_amendments']); ?>
-        <?php echo render($content['field_instrument_international_init']); ?>
-        <?php echo render($content['field_working_groups']); ?>
-        <?php echo render($content['field_member_of_big']); ?>
-    </tbody>
-</table>
+
+<!-- table moved to node--legal_instrument.tpl.php -->
 <?php
     }
 ?>
-
-
-<div class="clear">&nbsp;</div>
-<hr />
 
 <h4>
     <?php echo t('Countries'); ?>
@@ -105,9 +74,6 @@
     </tbody>
 </table>
 
-<div class="clearfix">&nbsp;</div>
-<br />
-<hr />
 <?php
     }
 ?>
