a848a9e1ca8f10e987554cdf5eacdce6f5e59628
Sonia Andriu <sonia.andriu@eaudeweb.ro>
2014-02-06 13:02:11 +0100

    Refs #1978, #1983, #1987: correct Bootstrap grid structure for species, project, country profiles

--- cms_frontend/templates/node/node--project.tpl.php
+++ cms_frontend/templates/node/node--project.tpl.php
@@ -1,98 +1,102 @@
 
-<div class="project-profile-left profile col-md-8">
-  <label>Project description</label>
-  <?php
-    echo render($content['body']);
-  ?>
-
-  <label>Conservation</label>
-  <div class="section">
-    <?php
-      echo render($content['field_project_conservation']);
-    ?>
-  </div>
-
-  <label>Objective</label>
-  <div class="section">
-    <?php
-      echo render($content['field_project_objective']);
-    ?>
-  </div>
-
-  <br />
-  <?php
-    render_slot($node, 'activity', 'project', $content);
-    echo render($content['field_project_file']);
-  ?>
-</div>
-
-
-<div class="project-profile-right well profile col-md-4">
-  <?php
-    render_slot($node, 'gallery', 'project');
-  ?>
-  <hr />
-
-  <table class="table table-condensed table-hover two-columns">
-    <tbody>
+<div class="container">
+  <div class="row">
+    <div class="project-profile-left profile col-md-8">
+      <label>Project description</label>
       <?php
-        echo render($content['field_project_impl_agency']);
+        echo render($content['body']);
+      ?>
 
-        if (property_exists($node, 'contacts') && !empty($node->contacts)) {
-      ?>
-      <tr>
-        <th><?php echo t('Implementing Agency contact'); ?></th>
-        <td>
-            <?php
-                foreach ($node->contacts as $contact) {
-                    echo '<a href="/contacts/item/' . $contact['uid'][0] . '/' . $contact['conventions'][0] . '/view">' . $contact['cn'][0]  . '</a>';
-                    echo '<br />';
-                }
-            ?>
-        </td>
-      </tr>
-      <?php
-          }
-          echo render($content['field_project_collab_agency']);
-      ?>
-    </tbody>
-  </table>
-  <hr />
+      <label>Conservation</label>
+      <div class="section">
+        <?php
+          echo render($content['field_project_conservation']);
+        ?>
+      </div>
 
-  <table class="table table-condensed table-hover two-columns">
-    <?php
-        echo render($content['field_project_start_date']);
-        echo render($content['field_project_end_date']);
-        echo render($content['field_project_appendix']);
-        echo render($content['field_project_taxonomic_group']);
-        echo render($content['field_project_region']);
-        echo render($content['field_project_country']);
-        echo render($content['field_project_tech_report']);
-    ?>
-  </table>
-  <hr />
-  
-  <label>Implementing Agency Contacts</label>
-  <div class="section">
-  <!-- to be implemented -->
-  </div>
-  <hr />
-  
-  <div class="threats">
-    <label>Threats</label>
-    <?php
-      if (check_display_field($content, 'field_project_threat')) {
-        echo render($content['field_project_threat']);
-      } else {
-    ?>
-    <p class="text-warning">
+      <label>Objective</label>
+      <div class="section">
+        <?php
+          echo render($content['field_project_objective']);
+        ?>
+      </div>
+
+      <br />
       <?php
-        echo t('No related threats');
+        render_slot($node, 'activity', 'project', $content);
+        echo render($content['field_project_file']);
       ?>
-    </p>
+    </div>
+
+
+    <div class="project-profile-right well profile col-md-4">
       <?php
-          }
+        render_slot($node, 'gallery', 'project');
       ?>
+      <hr />
+
+      <table class="table table-condensed table-hover two-columns">
+        <tbody>
+          <?php
+            echo render($content['field_project_impl_agency']);
+
+            if (property_exists($node, 'contacts') && !empty($node->contacts)) {
+          ?>
+          <tr>
+            <th><?php echo t('Implementing Agency contact'); ?></th>
+            <td>
+                <?php
+                    foreach ($node->contacts as $contact) {
+                        echo '<a href="/contacts/item/' . $contact['uid'][0] . '/' . $contact['conventions'][0] . '/view">' . $contact['cn'][0]  . '</a>';
+                        echo '<br />';
+                    }
+                ?>
+            </td>
+          </tr>
+          <?php
+              }
+              echo render($content['field_project_collab_agency']);
+          ?>
+        </tbody>
+      </table>
+      <hr />
+
+      <table class="table table-condensed table-hover two-columns">
+        <?php
+            echo render($content['field_project_start_date']);
+            echo render($content['field_project_end_date']);
+            echo render($content['field_project_appendix']);
+            echo render($content['field_project_taxonomic_group']);
+            echo render($content['field_project_region']);
+            echo render($content['field_project_country']);
+            echo render($content['field_project_tech_report']);
+        ?>
+      </table>
+      <hr />
+
+      <label>Implementing Agency Contacts</label>
+      <div class="section">
+      <!-- to be implemented -->
+      </div>
+      <hr />
+
+      <div class="threats">
+        <label>Threats</label>
+        <?php
+          if (check_display_field($content, 'field_project_threat')) {
+            echo render($content['field_project_threat']);
+          } else {
+        ?>
+        <p class="text-warning">
+          <?php
+            echo t('No related threats');
+          ?>
+        </p>
+          <?php
+              }
+          ?>
+      </div>
+    </div>
   </div>
 </div>
 
--- cms_frontend/templates/node/node--species.tpl.php
+++ cms_frontend/templates/node/node--species.tpl.php
@@ -4,30 +4,33 @@
     #render_slot($node, 'node-buttons-actions', 'general');
 ?>
 
-<div class="species-left-column profile col-md-8">
-<?php
-    render_slot($node, 'assessment', 'species', $content);
-    render_slot($node, 'geographic-range', 'species', $content);
-    render_slot($node, 'related-content', 'species', $content);
-?>
-</div>
+<div class="container">
+  <div class="row">
+    <div class="species-left-column profile col-md-8">
+    <?php
+        render_slot($node, 'assessment', 'species', $content);
+        render_slot($node, 'geographic-range', 'species', $content);
+        render_slot($node, 'related-content', 'species', $content);
+    ?>
+    </div>
 
-<div class="species-right-column profile well col-md-4">
-<?php
-    render_slot($node, 'gallery', 'species');
-    render_slot($node, 'common-names', 'species', $content);
-    render_slot($node, 'taxonomy', 'species', $content);
-    
-    
-    #render_slot($node, 'population', 'species', $content);
-    #render_slot($node, 'population-size', 'species', $content);
-    #render_slot($node, 'population-status', 'species', $content);
-    #render_slot($node, 'country-status', 'species', $content);
-    #render_slot($node, 'notes', 'species', $content);
+    <div class="species-right-column profile well col-md-4">
+    <?php
+        render_slot($node, 'gallery', 'species');
+        render_slot($node, 'common-names', 'species', $content);
+        render_slot($node, 'taxonomy', 'species', $content);
 
-    hide($content['links']);
-    hide($content['comments']);
+        #render_slot($node, 'population', 'species', $content);
+        #render_slot($node, 'population-size', 'species', $content);
+        #render_slot($node, 'population-status', 'species', $content);
+        #render_slot($node, 'country-status', 'species', $content);
+        #render_slot($node, 'notes', 'species', $content);
 
-    drupal_add_js(drupal_get_path('theme', $GLOBALS['theme']) . DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR . 'species.js');
-?>
+        hide($content['links']);
+        hide($content['comments']);
+
+        drupal_add_js(drupal_get_path('theme', $GLOBALS['theme']) . DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR . 'species.js');
+    ?>
+    </div>
+  </div>
 </div>
