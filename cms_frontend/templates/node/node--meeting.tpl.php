3e193ae7da46d3ab5a8b148ba17875b01041abb9
Sonia Andriu <sonia.andriu@eaudeweb.ro>
2014-02-06 11:20:45 +0100

    arrange meeting profile

--- cms_frontend/templates/node/node--meeting.tpl.php
+++ cms_frontend/templates/node/node--meeting.tpl.php
@@ -1,60 +1,73 @@
-<?php
-    render_slot($node, 'node-buttons', 'meeting');
-?>
 
-<div class="row">
-    <div class="span8">
-        <table class="table table-condensed table-hover two-columns">
-            <tbody>
-            <?php
-                echo render($content['field_meeting_start']);
-                echo render($content['field_meeting_start_time']);
-                echo render($content['field_meeting_end']);
-                echo render($content['field_meeting_reg_deadline']);
-                echo render($content['field_meeting_organizer']);
-                echo render($content['field_meeting_coorganizer']);
-                echo render($content['field_meeting_instrument']);
-                echo render($content['field_meeting_type']);
-                echo render($content['field_meeting_kind']);
-                echo render($content['field_meeting_status']);
-                echo render($content['field_meeting_languages']);
-                echo render($content['field_meeting_url']);
-
-            ?>
-            </tbody>
-        </table>
-        <div class="clearfix">&nbsp;</div>
-        <?php echo render($content['field_meeting_description']); ?>
-    </div>
-
-    <div class="span4">
-        <?php
-            echo render($content['field_meeting_image']);
-        ?>
-
-        <div class="thumbnail">
-            <div class="caption">
-                <table class="table table-condensed table-hover two-columns">
-                    <tbody>
-                    <?php
-                        echo render($content['field_meeting_country']);
-                        echo render($content['field_meeting_city']);
-                        echo render($content['field_meeting_address']);
-                    ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-
-    </div>
-
-    <div class="span12">
-        <?php echo render_slot($node, 'related-content', 'meeting', $content); ?>
-    </div>
-
+<div class="meeting-left profile col-md-8">
+  <?php echo render($content['field_meeting_description']); ?>
+  <label>[Please insert physical map here]</label>
 </div>
 
-<?php
+<div class="meeting-right well profile col-md-4">
+  <?php
+      echo render($content['field_meeting_image']);
+  ?>
+
+  <table class="table table-condensed table-hover two-columns">
+    <tbody>
+      <?php
+        echo render($content['field_meeting_start']);
+        echo render($content['field_meeting_start_time']);
+        echo render($content['field_meeting_end']);
+        echo render($content['field_meeting_reg_deadline']);
+        echo render($content['field_meeting_organizer']);
+        echo render($content['field_meeting_coorganizer']);
+        echo render($content['field_meeting_instrument']);
+        echo render($content['field_meeting_type']);
+        echo render($content['field_meeting_kind']);
+        echo render($content['field_meeting_status']);
+        echo render($content['field_meeting_languages']);
+        #echo render($content['field_meeting_url']);
+        echo render($content['field_meeting_country']);
+        echo render($content['field_meeting_city']);
+      ?>
+    </tbody>
+  </table>
+
+  <table class="table table-condensed table-hover two-columns">
+    <tbody>
+      <?php
+          echo render($content['field_meeting_address']);
+      ?>
+    </tbody>
+  </table>
+
+  <?php
     hide($content['links']);
     hide($content['comments']);
-?>
+  ?>
+</div>
+
+<div class="row">
+  <div class="meeting-full-width profile col-md-12">
+    <?php
+      if (!empty($node->field_meeting_document[$node->language])) {
+          $types = array();
+          foreach ($node->field_meeting_document[$node->language] as $document) {
+              if ($document['entity']->status == 1) {
+                  foreach ($document['entity']->field_document_type[$node->language] as $term) {
+                      if(!in_array($term['tid'], $types)) {
+                          $types []= $term['tid'];
+                      }
+                  }
+              }
+          }
+          foreach ($types as $tid) {
+              $type_term = taxonomy_term_load($tid);
+      ?>
+              <h4><?php echo $type_term->name; ?></h4>
+      <?php
+              print views_embed_view('meeting_documents_list_reorder','m_d_list', $node->nid, $tid);
+          }
+      }
+    ?>
+  </div>
+</div>
+
+
