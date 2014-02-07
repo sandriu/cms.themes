7ab56685821fdd9aea45b5c9e4f35a59c724e28c
Sonia Andriu <sonia.andriu@eaudeweb.ro>
2014-02-07 16:16:49 +0100

    move 2 php lines in left column

--- cms_frontend/templates/node/node--country.tpl.php
+++ cms_frontend/templates/node/node--country.tpl.php
@@ -2,17 +2,14 @@
 <div class="container">
   <div class="row">
     <div class="country-profile-left profile col-md-8">
-      <?php
 
-
-          hide($content['links']);
-          hide($content['comments']);
-      ?>
     </div>
 
     <div class="country-profile-right profile well col-md-4">
       <?php
           render_slot($node, 'details', 'cms_country', $content);
+          hide($content['links']);
+          hide($content['comments']);
       ?>
     </div>
   </div>
