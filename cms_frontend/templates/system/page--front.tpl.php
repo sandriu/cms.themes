dcde51c5d3c471d90b8d5f81408e41eabf6e1ab3
Sonia Andriu <sonia.andriu@eaudeweb.ro>
2014-02-13 18:14:07 +0100

    Refs #1889: partial style for instruments slideshow

--- cms_frontend/templates/system/page--front.tpl.php
+++ cms_frontend/templates/system/page--front.tpl.php
@@ -43,7 +43,8 @@
 
   <div class="row">
       <?php if (!empty($page['slide_instruments'])): ?>
-        <div class="region col-md-12 center-column">        
+        <div class="region col-md-12 center-column instruments-slideshow">
+          <h3>CMS instruments</h3>
             <?php print render($page['slide_instruments']); ?>          
         </div>
     <?php endif; ?>
