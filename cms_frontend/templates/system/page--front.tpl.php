4ac092501d5a5f062b536bca3b96cd701f055cbc
Sonia Andriu <sonia.andriu@eaudeweb.ro>
2014-02-13 15:36:42 +0100

    Refs #1889: remove breadcrumbs and page title from homepage

--- cms_frontend/templates/system/page--front.tpl.php
+++ cms_frontend/templates/system/page--front.tpl.php
@@ -10,11 +10,11 @@
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print '<div class="container">'.$breadcrumb.'</div>'; endif;?>
+
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
+
       <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
