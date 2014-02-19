38284d51c766a6d7157de204b6864400542635c1
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2014-02-19 15:34:13 +0200

    Update species related content slot in manage interface

--- cms_theme/templates/slots/species/related-content.tpl.php
+++ cms_theme/templates/slots/species/related-content.tpl.php
@@ -1,107 +1,136 @@
-<h3 class="muted">
-    <?php
-        echo t('Related content');
-    ?>
-</h3>
-
 <ul class="nav nav-tabs" id="related-content-tabs">
     <?php
-        render_tab(t('Meetings'), 'related-content-meetings', 'active', 'field_species_meeting', TRUE, $content);
-        render_tab(t('Publications'), 'related-content-publications', '', 'field_species_publication', TRUE, $content);
-        render_tab(t('Projects'), 'related-content-projects', '', 'field_species_project', TRUE, $content);
-        render_tab(t('National plans'), 'related-content-national-plans', '', 'field_species_plans', TRUE, $content);
-        render_tab(t('National reports'), 'related-content-national-reports', '', 'field_species_national_report', TRUE, $content);
-        render_tab(t('Other Documents'), 'related-content-species', '', 'field_species_document', TRUE, $content);
-        render_tab(t('Threats'), 'related-content-threats', '', 'field_species_threats', TRUE, $content);
-        render_tab(t('Contacts'), 'related-content-contacts', '', 'field_species_experts', TRUE, $content);
+    $first = 'active';
+    if ($node->meetings_count > 0) {
+        render_tab_view(t('Meetings'), 'related-content-meetings', $first, $node->meetings_count);
+        $first = '';
+    }
+
+    if ($node->publications_count > 0) {
+        render_tab_view(t('Publications'), 'related-content-publications', $first, $node->publications_count);
+        $first = '';
+    }
+
+    if ($node->projects_count > 0) {
+        render_tab_view(t('Projects'), 'related-content-projects', $first, $node->projects_count);
+        $first = '';
+    }
+
+    if ($node->national_plans_count > 0) {
+        render_tab_view(t('National plans'), 'related-content-national-plans', $first, $node->national_plans_count);
+        $first = '';
+    }
+
+    if ($node->national_reports_count > 0) {
+        render_tab_view(t('National reports'), 'related-content-national-reports', $first, $node->national_reports_count);
+        $first = '';
+    }
+
+    if ($node->other_documents_count > 0) {
+        render_tab_view(t('Other Documents'), 'related-content-other-documents', $first, $node->other_documents_count);
+        $first = '';
+    }
+    render_tab(t('Threats'), 'related-content-threats', '', 'field_species_threats', TRUE, $content);
+    render_tab(t('Contacts'), 'related-content-contacts', '', 'field_species_experts', TRUE, $content);
     ?>
 </ul>
 
 <div class="tab-content">
-    <div class="tab-pane active loaded" id="related-content-meetings">
-        <?php
+    <?php if ($node->meetings_count > 0) { ?>
+        <div class="tab-pane active loaded" id="related-content-meetings">
+            <?php
             echo views_embed_view('meetings', 'species_meetings', $node->nid);
-        ?>
-    </div>
+            ?>
+        </div>
+    <?php } ?>
 
-    <div class="tab-pane" id="related-content-publications">
-        <?php
+    <?php if ($node->publications_count > 0) { ?>
+        <div class="tab-pane" id="related-content-publications">
+            <?php
             echo views_embed_view('publications_admin', 'species_publications', $node->nid);
-        ?>
-    </div>
+            ?>
+        </div>
+    <?php } ?>
 
-    <div class="tab-pane" id="related-content-projects">
-        <?php
-        echo views_embed_view('project_admin', 'species_projects', $node->nid);
-        ?>
-    </div>
+    <?php if ($node->projects_count > 0) { ?>
+        <div class="tab-pane" id="related-content-projects">
+            <?php
+            echo views_embed_view('project_admin', 'species_projects', $node->nid);
+            ?>
+        </div>
+    <?php } ?>
 
-    <div class="tab-pane" id="related-content-national-plans">
-        <?php
+    <?php if ($node->national_plans_count > 0) { ?>
+        <div class="tab-pane" id="related-content-national-plans">
+            <?php
             echo views_embed_view('documents', 'species_plans', $node->nid);
-        ?>
-    </div>
+            ?>
+        </div>
+    <?php } ?>
 
-    <div class="tab-pane" id="related-content-national-reports">
-        <?php
+    <?php if ($node->national_reports_count > 0) { ?>
+        <div class="tab-pane" id="related-content-national-reports">
+            <?php
             echo views_embed_view('documents', 'species_national_reports', $node->nid);
-        ?>
-    </div>
+            ?>
+        </div>
+    <?php } ?>
 
-    <div class="tab-pane" id="related-content-species">
-        <?php
+    <?php if ($node->other_documents_count > 0) { ?>
+        <div class="tab-pane" id="related-other-documents">
+            <?php
             echo views_embed_view('documents', 'species_other_documents', $node->nid);
-        ?>
-    </div>
-
+            ?>
+        </div>
+    <?php } ?>
     <div class="tab-pane" id="related-content-threats">
         <?php
-            if (check_display_field($content, 'field_species_threats')) {
-        ?>
-        <table class="table table-condensed table-hover table-bordered table-striped">
-            <caption><?php echo t('Threats'); ?></caption>
-            <thead>
+        if (check_display_field($content, 'field_species_threats')) {
+            ?>
+            <table class="table table-condensed table-hover table-bordered table-striped">
+                <caption><?php echo t('Threats'); ?></caption>
+                <thead>
                 <tr>
                     <th><?php echo t('Threat'); ?></th>
                     <th><?php echo t('Notes'); ?></th>
                 </tr>
-            </thead>
+                </thead>
 
-            <tbody>
+                <tbody>
                 <?php echo render($content['field_species_threats']); ?>
-            </tbody>
-        </table>
+                </tbody>
+            </table>
         <?php
-            }else {
-        ?>
-        <p class="text-warning">
-        <?php
+        }else {
+            ?>
+            <p class="text-warning">
+                <?php
                 echo t('No related threats');
-        ?>
-        </p>
+                ?>
+            </p>
         <?php
-            }
+        }
         ?>
     </div>
 
     <div class="tab-pane" id="related-content-contacts">
         <?php
-            if (isset($node->experts) && (!empty($node->experts))) {
+        if (isset($node->experts) && (!empty($node->experts))) {
 //                foreach ($node->experts as $expert) {
 //                    echo '<a href="/' . ADMINISTRATION_PATH . 'contacts/item/' . $expert['uid'][0] . '/' . $expert['conventions'][0] . '/view">' . $expert['cn'][0]  . '</a>';
 //                    echo '<br />';
 //                }
-        ?>
+            ?>
         <?php
-            }else {
-        ?>
-        <p class="text-warning">
-        <?php
+        }else {
+            ?>
+            <p class="text-warning">
+                <?php
                 echo t('No experts');
-        ?>
-        </p>
+                ?>
+            </p>
         <?php
-            }
+        }
         ?>
     </div>
 </div>
