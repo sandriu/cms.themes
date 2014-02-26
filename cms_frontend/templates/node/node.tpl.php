0c8d02b75a900f620a0f6a01c5aea47ac4f05a99
rciofringeanu <radu.sc@gmail.com>
2014-02-26 16:18:49 +0200

    Alter data display for teaser

--- cms_frontend/templates/node/node.tpl.php
+++ cms_frontend/templates/node/node.tpl.php
@@ -114,8 +114,8 @@
       hide($content['comments']);
       hide($content['links']);
       print render($content);      
-    ?>
-      <span class="text-muted">Last updated on <?php print format_date($node->changed,'custom','d F Y'); ?></span>
+    ?>      
+      <span class="text-muted"><?php if(!$teaser)print 'Last updated on'; ?> <?php print format_date($node->changed,'custom','d F Y'); ?></span>
   </div>
 
   <?php print render($content['links']); ?>
