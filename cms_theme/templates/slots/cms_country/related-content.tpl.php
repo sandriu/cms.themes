305a25e8a26d7818e66bb16ff8ad3e72c31b8520
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2014-03-04 20:02:52 +0200

    Refactored related content for Country and Document in Manage theme

--- cms_theme/templates/slots/cms_country/related-content.tpl.php
+++ cms_theme/templates/slots/cms_country/related-content.tpl.php
@@ -5,75 +5,82 @@
 </h3>
 
 <ul class="nav nav-tabs" id="related-content-tabs">
-    <?php
-        $total_reports = 0;
-        $submmiter = taxonomy_get_term_by_name($node->title, VOC_DOCUMENT_SUBMITTER);
-        if ($submmiter) {
-            $country_term_id = key($submmiter);
-            $total_reports = count(views_get_view_result('related_national_reports_country', 'page', $country_term_id));
-        }
+<?php
+    $first_tab = 'active';
+    if (!empty($node->related_data['species']['count'])) {
 
-        render_tab(t('Meetings'), 'related-content-meetings', 'active', 'meetings', TRUE, $content);
-        render_tab(t('Projects'), 'related-content-projects', '', 'projects', TRUE, $content);
-    ?>
-    <li class="">
-        <a href="#related-content-national-reports" data-toggle="tab">
-            <?php
-                echo t('National Reports');
-            ?>
-            (<?php echo $total_reports; ?>)
-        </a>
-    </li>
-    <?php
-        render_tab(t('National Focal Points'), 'related-content-nfp', '', 'national_focal_points', TRUE, $content);
-        render_tab(t('Ratification status'), 'ratification-status', '', 'field_country_instrument_status', FALSE, $content);
-        render_tab(t('Species'), 'related-content-species', '', 'species', TRUE, $content);
-    ?>
+        render_tab_view(t('Species'), 'related-content-species', $first_tab, $node->related_data['species']['count']);
+        $first_tab = '';
+    }
+
+    if (!empty($node->related_data['meetings']['count'])) {
+        render_tab_view(t('Meetings'), 'related-content-meetings', $first_tab, $node->related_data['meetings']['count']);
+        $first_tab = '';
+    }
+
+    if (!empty($node->related_data['projects']['count'])) {
+        render_tab_view(t('Projects'), 'related-content-projects', $first_tab, $node->related_data['projects']['count']);
+        $first_tab = '';
+    }
+
+    if (!empty($node->related_data['national_reports']['count'])) {
+        render_tab_view(t('National Reports'), 'related-content-national-reports', $first_tab, $node->related_data['national_reports']['count']);
+        $first_tab = '';
+    }
+
+    if (!empty($node->field_country_instrument_status)) {
+        render_tab_view(t('Ratification status'), 'ratification-status', $first_tab, '');
+        $first_tab = '';
+    }
+
+    if (!empty($node->national_focal_points)) {
+        render_tab_view(t('National Focal Points'), 'related-content-nfp', $first_tab, count($node->national_focal_points));
+        $first_tab = '';
+    }
+?>
 </ul>
 
 <div class="tab-content">
-    <div class="tab-pane active loaded" id="related-content-meetings">
-        <?php
-            echo views_embed_view('meetings', 'country_meetings', $node->nid);
-        ?>
-    </div>
+<?php
+    $first_tab = 'active loaded';
+    if (!empty($node->related_data['species']['count'])) {
+        render_tab_content_view('related-content-species', $first_tab,
+            $node->related_data['species']['view_name'], $node->related_data['species']['view_display'], $node->nid);
+        $first_tab = '';
+    }
 
-    <div class="tab-pane" id="related-content-projects">
-        <?php
-            echo views_embed_view('project_admin', 'country_projects', $node->nid);
-        ?>
-    </div>
+    if (!empty($node->related_data['meetings']['count'])) {
+        render_tab_content_view('related-content-meetings', $first_tab,
+            $node->related_data['meetings']['view_name'], $node->related_data['meetings']['view_display'], $node->nid);
+        $first_tab = '';
+    }
 
-    <div class="tab-pane" id="related-content-national-reports">
-        <?php
-            if ($submmiter) {
-                echo views_embed_view('documents', 'country_national_reports', $country_term_id);
-            }else {
-                echo t('No National reports available');
-            }
-        ?>
-    </div>
 
-    <div class="tab-pane" id="related-content-nfp">
-        <?php echo render($content['national_focal_points']); ?>
-    </div>
+    if (!empty($node->related_data['projects']['count'])) {
+        render_tab_content_view('related-content-projects', $first_tab,
+            $node->related_data['projects']['view_name'], $node->related_data['projects']['view_display'], $node->nid);
 
-    <div class="tab-pane" id="ratification-status">
-        <?php
-            if (check_display_field($content, 'field_country_instrument_status')) {
-                echo render($content['field_country_instrument_status']);
-            }else {
-        ?>
-        <p class="text-warning">
-        <?php
-                echo t('No ratification available');
-        ?>
-        </p>
-        <?php
-            }
-        ?>
-    </div>
-    <div class="tab-pane" id="related-content-species">
-        <?php echo render($content['species']); ?>
-    </div>
+        $first_tab = '';
+    }
+    if (!empty($node->related_data['national_reports']['count'])) {
+        render_tab_content_view('related-content-national-reports', $first_tab,
+            $node->related_data['national_reports']['view_name'], $node->related_data['national_reports']['view_display'], $node->nid);
+        $first_tab = '';
+    }
+?>
+
+    <?php if (!empty($node->field_country_instrument_status)) { ?>
+        <div class="tab-pane" id="ratification-status">
+            <?php echo render($content['field_country_instrument_status']); ?>
+        </div>
+        <?php $first_tab = ''; ?>
+    <?php } ?>
+
+    <?php if (!empty($node->national_focal_points)) { ?>
+        <div class="tab-pane" id="related-content-nfp">
+            <?php echo render($content['national_focal_points']); ?>
+        </div>
+        <?php $first_tab = ''; ?>
+    <?php } ?>
+
 </div>
