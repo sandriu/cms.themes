b74d9e6b42f7b4371a94b51854e810f7e4cbc4ac
rciofringeanu <radu.sc@gmail.com>
2014-03-08 14:51:01 +0200

    Add news image field

--- cms_frontend/templates/node/node--news.tpl.php
+++ cms_frontend/templates/node/node--news.tpl.php
@@ -1,16 +1,17 @@
 <div class="container">
-  <div class="row">
-    <div class="news-profile-left profile col-md-8">
-      <?php echo render($content['body']); ?> 
-      <span class="text-muted">
-          <?php if(!$teaser)print 'Last updated on'; ?> <?php print format_date($node->changed,'custom','d F Y'); ?>
-      </span>      
-    </div>
+    <div class="row">
+        <div class="news-profile-left profile col-md-8">
+            <?php echo render($content['body']); ?> 
+            <?php echo render($content['field_featured_image']); ?> 
+            <span class="text-muted">
+                <?php if (!$teaser) print 'Last updated on'; ?> <?php print format_date($node->changed, 'custom', 'd F Y'); ?>
+            </span>      
+        </div>
 
-    <div class="news-profile-right profile well col-md-4">
-      <?php
-          render_slot($node, 'details', 'news', $content);
-      ?>
-    </div>
-  </div> 
+        <div class="news-profile-right profile well col-md-4">
+            <?php
+            render_slot($node, 'details', 'news', $content);
+            ?>
+        </div>
+    </div> 
 </div>
