a29ff57f735590acd7a4bb175c252bced80dc4ec
rciofringeanu <radu.sc@gmail.com>
2014-03-10 11:50:00 +0200

    Add campaign template and slots

---
+++ cms_frontend/templates/slots/campaign/details.tpl.php
@@ -0,0 +1,17 @@
+<table class="table table-condensed table-hover two-columns">
+    <tbody>
+        <?php echo render($content['title_field']); ?>        
+        <?php echo render($content['field_campaign_sponsor']); ?>
+        <?php echo render($content['field_campaign_patron']); ?>
+        <?php echo render($content['field_campaign_type']); ?>
+        <?php echo render($content['field_campaign_duration']); ?>
+        <?php echo render($content['field_campaign_link']); ?>
+        <?php echo render($content['field_country']); ?>
+        <?php echo render($content['field_instrument']); ?>
+        <?php echo render($content['field_campaign_species']); ?>
+        <?php echo render($content['field_campaign_projects']); ?>
+        <?php echo render($content['field_campaign_publications']); ?>
+        <?php echo render($content['field_campaign_meetings']); ?>
+        <?php echo render($content['field_campaign_documents']); ?>        
+    </tbody>
+</table>
