52b07bfa84016c59cf7b8f65570331167fbf0f4d
rciofringeanu <radu.sc@gmail.com>
2014-03-10 12:10:58 +0200

    Add award template and slots

---
+++ cms_frontend/templates/slots/award/details.tpl.php
@@ -0,0 +1,13 @@
+<table class="table table-condensed table-hover two-columns">
+    <tbody>
+        <?php echo render($content['title_field']); ?>        
+        <?php echo render($content['field_instrument']); ?>
+        <?php echo render($content['field_award_recipient']); ?>
+        <?php echo render($content['field_award_sponsor']); ?>
+        <?php echo render($content['field_award_eligibility']); ?>
+        <?php echo render($content['field_award_application']); ?>
+        <?php echo render($content['field_award_species']); ?>
+        <?php echo render($content['field_award_meetings']); ?>
+        <?php echo render($content['field_award_documents']); ?>        
+    </tbody>
+</table>
