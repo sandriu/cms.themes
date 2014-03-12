870b16e83e4e410900679bc164c7de716fda3f6e
rciofringeanu <radu.sc@gmail.com>
2014-03-12 17:04:13 +0200

    Add IE css files

--- cms_frontend/template.php
+++ cms_frontend/template.php
@@ -15,11 +15,11 @@ function cms_frontend_preprocess_html(&$variables) {
           'http://fonts.googleapis.com/css?family=Lato:400,700,900,400italic', array('type' => 'external')
   );
   // Add conditional CSS for IE8
-  drupal_add_css(path_to_theme() . '/css/ie8.css', array('media'=>'screen','group' => CSS_THEME, 'browsers' => array('IE' => 'IE 8', '!IE' => FALSE), 'weight' => 999, 'preprocess' => FALSE));
+  drupal_add_css(path_to_theme() . '/css/ie/ie8_custom.css', array('media'=>'screen','group' => CSS_THEME, 'browsers' => array('IE' => 'IE 8', '!IE' => FALSE), 'weight' => 999, 'preprocess' => FALSE));
   // Add conditional CSS for IE9
-  drupal_add_css(path_to_theme() . '/css/ie9.css', array('media'=>'screen','group' => CSS_THEME, 'browsers' => array('IE' => 'IE 9', '!IE' => FALSE), 'weight' => 999, 'preprocess' => FALSE));
+  drupal_add_css(path_to_theme() . '/css/ie/ie9_custom.css', array('media'=>'screen','group' => CSS_THEME, 'browsers' => array('IE' => 'IE 9', '!IE' => FALSE), 'weight' => 999, 'preprocess' => FALSE));
   // Add conditional CSS for IE10
-  drupal_add_css(path_to_theme() . '/css/ie10.css', array('media'=>'screen','group' => CSS_THEME, 'browsers' => array('IE' => 'IE 10', '!IE' => FALSE), 'weight' => 999, 'preprocess' => FALSE));
+  drupal_add_css(path_to_theme() . '/css/ie/ie10.css', array('media'=>'screen','group' => CSS_THEME, 'browsers' => array('IE' => 'IE 10', '!IE' => FALSE), 'weight' => 999, 'preprocess' => FALSE));
 
   drupal_add_js(array('cms_front_end' => array('is_front_page' => drupal_is_front_page() )), 'setting');
   //Ierarhic menu
