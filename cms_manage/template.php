87f85d59d266f998753a1d4effb0b03f9049ae54
Cătălin Dogaru <catalin.dogaru@gmail.com>
2014-03-27 18:34:46 +0200

    Added custom table styles

--- cms_manage/template.php
+++ cms_manage/template.php
@@ -1,5 +1,20 @@
 <?php
-
 /**
  * @file template.php
  */
+
+
+/**
+ * Implements template_preprocess_views_view_table().
+ *
+ * @param $vars
+ *   An associative array with generated variables.
+ *
+ * @return
+ *   Nothing.
+ */
+function cms_manage_preprocess_views_view_table(&$vars) {
+  $vars['classes_array'][] = 'table-bordered';
+  $vars['classes_array'][] = 'table-hover';
+  $vars['classes_array'][] = 'table-striped';
+}
