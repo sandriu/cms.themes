87d84d9334db77280a51f81aba4a498d176b41e0
Cătălin Dogaru <catalin.dogaru@gmail.com>
2014-04-15 12:52:50 +0300

    Updated the theme path

--- cms_manage/templates/node/node--legal_instrument.tpl.php
+++ cms_manage/templates/node/node--legal_instrument.tpl.php
@@ -18,5 +18,5 @@
 <script type="text/javascript" src="/<?php echo $DT_js_path; ?>"></script>
 <link type="text/css" rel="stylesheet" href="/<?php echo $css_path; ?>" />
 <?php
-    drupal_add_js(drupal_get_path('theme', 'cms_theme') . DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR . 'simple_datatables.js');
+    drupal_add_js(drupal_get_path('theme', 'cms_manage') . DIRECTORY_SEPARATOR . 'js' . DIRECTORY_SEPARATOR . 'simple_datatables.js');
 ?>
