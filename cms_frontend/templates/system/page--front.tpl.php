d66bb1e96210df09c5ba6c773a64f399c290b170
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2014-04-17 19:48:20 +0300

    Mobile menu positioned from top of the page

--- cms_frontend/templates/system/page--front.tpl.php
+++ cms_frontend/templates/system/page--front.tpl.php
@@ -1,64 +1,60 @@
-<!-- page header -->
-<?php require "page-header.tpl.php"; ?>
-
-<!-- page container -->
-<div class="container">
-  <div class="row row-offcanvas row-offcanvas-left">
-
-    <?php if (!empty($page['mobile_menu'])): ?>
-        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" role="navigation">
-            <nav role="navigation">
-                <?php if (!empty($page['mobile_menu'])): ?>
-                    <?php print render($page['mobile_menu']); ?>
-                <?php endif; ?>
-            </nav>
-        </div>
-    <?php endif; ?>
-
-    <!-- center column -->
-    <div class="region col-md-8 center-column">
-      <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
-      <?php endif; ?>
-
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-    </div>
-
-    <!-- right column-->
-    <?php if (!empty($page['sidebar_second'])): ?>
-    <aside class="col-md-4 right-column" role="complementary">
-      <div class="well region region-sidebar-second">
-        <?php print render($page['sidebar_second']); ?>
-      </div>
-    </aside>
-    <?php endif; ?>
-
-  </div>
-
-  <div class="row">
-      <?php if (!empty($page['slide_instruments'])): ?>
-        <div class="region col-md-12 center-column instruments-slideshow">
-            <?php print render($page['slide_instruments']); ?>
-        </div>
-    <?php endif; ?>
-  </div>
+<div class="col-xs-6 col-sm-3 sidebar-offcanvas" role="mobile-navigation">
+    <nav role="mobile-navigation">
+        <ul id="mobile-main-menu" class="menu nav">
+        </ul>
+    </nav>
 </div>
 
-<!-- page footer -->
-<?php require "page-footer.tpl.php"; ?>
+<div class="row row-offcanvas row-offcanvas-left">
+    <!-- page header -->
+    <?php require "page-header.tpl.php"; ?>
+
+    <!-- page container -->
+    <div class="container">
+        <!-- center column -->
+        <div class="region col-md-8 center-column">
+          <?php if (!empty($page['highlighted'])): ?>
+            <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
+          <?php endif; ?>
+
+          <a id="main-content"></a>
+          <?php print render($title_prefix); ?>
+          <?php if (!empty($title)): ?>
+
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
+          <?php print $messages; ?>
+          <?php if (!empty($tabs)): ?>
+            <?php print render($tabs); ?>
+          <?php endif; ?>
+          <?php if (!empty($page['help'])): ?>
+            <?php print render($page['help']); ?>
+          <?php endif; ?>
+          <?php if (!empty($action_links)): ?>
+            <ul class="action-links"><?php print render($action_links); ?></ul>
+          <?php endif; ?>
+          <?php print render($page['content']); ?>
+        </div>
+
+        <!-- right column-->
+        <?php if (!empty($page['sidebar_second'])): ?>
+        <aside class="col-md-4 right-column" role="complementary">
+          <div class="well region region-sidebar-second">
+            <?php print render($page['sidebar_second']); ?>
+          </div>
+        </aside>
+        <?php endif; ?>
+
+      </div>
+
+      <div class="row">
+          <?php if (!empty($page['slide_instruments'])): ?>
+            <div class="region col-md-12 center-column instruments-slideshow">
+                <?php print render($page['slide_instruments']); ?>
+            </div>
+        <?php endif; ?>
+      </div>
+
+    <!-- page footer -->
+    <?php require "page-footer.tpl.php"; ?>
+</div>
--- cms_frontend/templates/system/page.tpl.php
+++ cms_frontend/templates/system/page.tpl.php
@@ -1,57 +1,51 @@
-<!-- page header -->
-<?php require "page-header.tpl.php"; ?>
+<div class="col-xs-6 col-sm-3 sidebar-offcanvas" role="mobile-navigation">
+    <nav role="mobile-navigation">
+        <ul id="mobile-main-menu" class="menu nav">
+        </ul>
+    </nav>
+</div>
+<div class="row-offcanvas row-offcanvas-left">
+    <!-- page header -->
+    <?php require "page-header.tpl.php"; ?>
 
-<!-- page container -->
-<div class="container">
-  <div class="row row-offcanvas row-offcanvas-left">
-
-      <?php if (!empty($page['mobile_menu'])): ?>
-          <div class="col-xs-6 col-sm-3 sidebar-offcanvas" role="navigation">
-              <nav role="navigation">
-                  <?php if (!empty($page['mobile_menu'])): ?>
-                      <?php print render($page['mobile_menu']); ?>
-                  <?php endif; ?>
-              </nav>
-          </div>
-      <?php endif; ?>
-
-      <!-- center column -->
-    <div class="region col-md-12 center-column">
-      <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
-      <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print '<div class="container">'.$breadcrumb.'</div>'; endif;?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-      <?php endif; ?>
-      <?php if (!empty($page['help'])): ?>
-        <?php print render($page['help']); ?>
-      <?php endif; ?>
-      <?php if (!empty($action_links)): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php
-        if(!empty($page['before_content'])) {
-          print render($page['before_content']);
-        }
-      ?>
-      <?php print render($page['content']); ?>
-      <?php
-        if(!empty($page['after_content'])) {
-          print render($page['after_content']);
-        }
-      ?>
+    <!-- page container -->
+    <div class="container">
+          <!-- center column -->
+        <div class="region col-md-12 center-column">
+          <?php if (!empty($page['highlighted'])): ?>
+            <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
+          <?php endif; ?>
+          <?php if (!empty($breadcrumb)): print '<div class="container">'.$breadcrumb.'</div>'; endif;?>
+          <a id="main-content"></a>
+          <?php print render($title_prefix); ?>
+          <?php if (!empty($title)): ?>
+            <h1 class="page-header"><?php print $title; ?></h1>
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
+          <?php print $messages; ?>
+          <?php if (!empty($tabs)): ?>
+            <?php print render($tabs); ?>
+          <?php endif; ?>
+          <?php if (!empty($page['help'])): ?>
+            <?php print render($page['help']); ?>
+          <?php endif; ?>
+          <?php if (!empty($action_links)): ?>
+            <ul class="action-links"><?php print render($action_links); ?></ul>
+          <?php endif; ?>
+          <?php
+            if(!empty($page['before_content'])) {
+              print render($page['before_content']);
+            }
+          ?>
+          <?php print render($page['content']); ?>
+          <?php
+            if(!empty($page['after_content'])) {
+              print render($page['after_content']);
+            }
+          ?>
+        </div>
     </div>
 
-  </div>
+    <!-- page footer -->
+    <?php require "page-footer.tpl.php"; ?>
 </div>
-
-<!-- page footer -->
-<?php require "page-footer.tpl.php"; ?>
