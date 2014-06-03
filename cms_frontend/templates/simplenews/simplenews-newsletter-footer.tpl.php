49cef08168f23aeaa4ca5bc44bac6eff4ef547b2
Cătălin Dogaru <catalin.dogaru@gmail.com>
2014-06-03 14:34:22 +0300

    [refs #2374] Updated the newsletter footer

--- cms_frontend/templates/simplenews/simplenews-newsletter-footer.tpl.php
+++ cms_frontend/templates/simplenews/simplenews-newsletter-footer.tpl.php
@@ -31,7 +31,7 @@
 $template_path = drupal_get_path('theme', 'cms_frontend');
 $template_url = url($template_path, array('absolute' => TRUE, 'language' => LANGUAGE_NONE));
 ?>
-<table border="0" cellpadding="0" cellspacing="0" width="600">
+<table border="0" cellpadding="0" cellspacing="0" width="600" style="background-color: #eee;">
   <tbody>
     <?php if (!$opt_out_hidden): ?>
       <tr style="background-color: #eee; font-size: 11px; color: #555; border-bottom: 1px solid #ddd; font-family: Arial, sans-serif;">
@@ -47,10 +47,7 @@ $template_url = url($template_path, array('absolute' => TRUE, 'language' => LANG
       </tr>
       <tr style="background-color: #eee; font-size: 11px; color: #555; font-family: Arial, sans-serif;">
         <td style="width: 470px; padding-top: 10px; padding-bottom: 10px; padding-left: 20px; padding-right: 20px; line-height: 1.4;">
-          <p><?php echo t('This newsletter is published by the CMS Secretariat &copy; 2014 UNEP / CMS'); ?></p>
-          <p><?php echo t('UNEP/CMS Secretariat, Platz der Vereinten Nationen 1, 53113 Bonn, Germany'); ?></p>
-          <p><?php echo t('Tel: (+49) 228 815 2413, Fax: (+49) 228 815 2450, E-mail: secretariat@cms.int'); ?></p>
-          <p><a href="<?php echo t('http://www.cms.int/'); ?>" style="color: #0066c0;"><?php echo t('www.cms.int'); ?></a>&nbsp;&nbsp;<a href="<?php echo t('http://www.unep.org/'); ?>" style="color: #0066c0;"><?php echo t('www.unep.org'); ?></a></p>
+          <p><?php echo t('This newsletter is published by the CMS Secretariat &copy; 2014 UNEP / CMS'); ?><br /><p><?php echo t('UNEP/CMS Secretariat, Platz der Vereinten Nationen 1, 53113 Bonn, Germany'); ?><br /><p><?php echo t('Tel: (+49) 228 815 2413, Fax: (+49) 228 815 2450, E-mail: secretariat@cms.int'); ?><br /><a href="<?php echo t('http://www.cms.int/'); ?>" style="color: #0066c0;"><?php echo t('www.cms.int'); ?></a>&nbsp;&nbsp;<a href="<?php echo t('http://www.unep.org/'); ?>" style="color: #0066c0;"><?php echo t('www.unep.org'); ?></a></p>
         </td>
         <td style="width: 130px; height: 100%; text-align: right; vertical-align: bottom; padding-bottom: 11px; padding-right: 25px;">
           <a href="[simplenews-subscriber:unsubscribe-url]" style="color: #0066c0;"><?php print $unsubscribe_text ?></a>
@@ -59,7 +56,7 @@ $template_url = url($template_path, array('absolute' => TRUE, 'language' => LANG
     <?php endif; ?>
     <?php if ($key == 'test'): ?>
       <tr style="background-color: #eee; font-size: 11px; color: #555; font-family: Arial, sans-serif;">
-        <td style="width: 470px; padding-top: 10px; padding-bottom: 10px; padding-left: 20px; padding-right: 20px; line-height: 1.4;">
+        <td colspan="2" style="width: 470px; padding-top: 10px; padding-bottom: 10px; padding-left: 20px; padding-right: 20px; line-height: 1.4;">
           <p><?php print $test_message; ?></p>
         </td>
       </tr>
